0d60739e215ba0453082b283636c7e4e77b90c9c
Kristin-98 <kristi98_s@hotmail.se>
2025-08-20 17:23:17 +0200

    placeholder + button

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import CounterButton from "./components/CounterButton";
 import TodoList from "./components/TodoList";
 
 function App() {
-  const myTodos = ["köp 1", "köp 2"];
-
   return (
     <div>
       <h1>Vite + React</h1>
 
       <CounterButton />
-      <TodoList todos={myTodos} />
+      <TodoList initialTodos={[]} />
     </div>
   );
 }
--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import TodoItem from "./TodoItem";
 
 type TodoListProps = {
-  todos: string[];
+  initialTodos?: string[];
 };
 
-function TodoList({ todos }: TodoListProps) {
+function TodoList({ initialTodos = [] }: TodoListProps) {
+  const [todos, setTodos] = useState<string[]>(initialTodos);
+  const [newTodo, setNewTodo] = useState("");
+
+  const addTodo = () => {
+    if (newTodo.trim() === "") return;
+    setTodos([...todos, newTodo]);
+    setNewTodo("");
+  };
+
   return (
     <div>
-      <h1>todo lista rubrik</h1>;
+      <h1>todo lista rubrik</h1>
       <ul>
         {todos.map((todo, index) => (
           <TodoItem key={index} text={todo} />
         ))}
       </ul>
+      <input
+        type="
+      text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Skriv ny todooooo"
+      />
+      <button onClick={addTodo}>Lägg till din todo</button>
     </div>
   );
 }
